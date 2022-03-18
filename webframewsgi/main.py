f27b5b3b982ebcdaf5667459000a980868a6ad62
antipod237 <antipod2370@gmail.com>
2022-03-18 16:24:13 +0300

    unused commit

--- webframewsgi/main.py
+++ webframewsgi/main.py
@@ -14,22 +14,29 @@ class WebFrame:
 
     def __call__(self, environ, start_response):
         from pprint import pprint; pprint(environ)
-        data = b"Hello, world\n"
+        raw_url = environ['PATH_INFO']
+        view = self._find_view(raw_url)()
+        method = environ['REQUEST_METHOD'].lower()
+        if not hasattr(view, method):
+            raise NotAllowed
+        raw_responce = getattr(view, method)(None)
+        responce = raw_responce.encode('utf-8')
+        #responce = b"Hello, world\n"
         start_response('200 OK', [
             ('Content-Type', 'text/plain'),
-            ('content-Length', str(len(data)))
+            ('content-Length', str(len(responce)))
         ])
-        return iter([data])
+        return iter([responce])
 
-        def _prepare_url(self, url: str):
-            if url[-1] == '/':
-                return url[:-1]
-            return url
+    def _prepare_url(self, url: str):
+        if url[-1] == '/':
+            return url[:-1]
+        return url
 
-        def _find_view(self, raw_url: str) -> Type(View):
-            url = self._prepare_url(raw_url)
-            for path in self.urls:
-                m = re.match(path.url, url)
-                if m is not None:
-                    return path.view
-                raise NotFound
+    def _find_view(self, raw_url: str) -> Type(View):
+        url = self._prepare_url(raw_url)
+        for path in self.urls:
+            m = re.match(path.url, url)
+            if m is not None:
+                return path.view
+            raise NotFound
